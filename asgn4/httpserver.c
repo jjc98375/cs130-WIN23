6bd7bd28af05c13b3905396fed0f050754327ce0
joshua <jcho129@ucsc.edu>
2023-03-14 06:06:52 +0000

    woigjw

--- asgn4/httpserver.c
+++ asgn4/httpserver.c
@@ -39,8 +39,8 @@ static queue_t *Q;
 pthread_mutex_t mutex;
 
 //audit function
-void audit(const char *method, const char *uri, uint16_t statusCode, int thread_id) {
-    fprintf(stderr, "%s,%s,%hu,%d\n", method, uri, statusCode, thread_id);
+void audit(const char *method, const char *uri, int statusCode, int thread_id) {
+    fprintf(stderr, "%s,%s,%d,%d\n", method, uri, statusCode, thread_id);
 }
 
 //WORKER THREAD
@@ -56,9 +56,9 @@ void *worker_thread(void *arg) {
 
         // if (connfd != -1) {
         // fprintf(stderr, "you connected it successfully\n");
-            fprintf(stderr, "WTF\n");
+        // fprintf(stderr, "WTF\n");
 
-        handle_connection((int)connfd);
+        handle_connection(connfd);
         // }
         close(connfd);
     }
@@ -73,8 +73,8 @@ int main(int argc, char **argv) {
 
     while ((opt = getopt(argc, argv, "t:")) != -1) {
         switch (opt) {
-            case 't': num_of_thread = atoi(optarg); break;
-            default: fprintf(stderr, "Usage: %s [-t threads] <port>\n", argv[0]); exit(EXIT_FAILURE);
+        case 't': num_of_thread = atoi(optarg); break;
+        default: fprintf(stderr, "Usage: %s [-t threads] <port>\n", argv[0]); exit(EXIT_FAILURE);
         }
     }
 
@@ -93,7 +93,7 @@ int main(int argc, char **argv) {
     pthread_t threads[num_of_thread];
 
     for (int i = 0; i < num_of_thread; i++) {
-    //    fprintf(stderr, "thread %d\n", i);
+        //    fprintf(stderr, "thread %d\n", i);
         if (pthread_create(&threads[i], NULL, worker_thread, NULL) != 0) {
             errx(EXIT_FAILURE, "pthread_create() failed");
         }
@@ -122,9 +122,6 @@ int main(int argc, char **argv) {
         };
     }
 
-
-
-
     //FINISH THE THREADS
     // for (int i = 0; i < num_of_thread; i++) {
     //     pthread_join(threads[i], NULL);
@@ -135,17 +132,17 @@ int main(int argc, char **argv) {
 }
 
 void handle_connection(int connfd) {
-    fprintf(stderr, "in handle connection\n");
+    // fprintf(stderr, "in handle connection\n");
 
     conn_t *conn = conn_new(connfd);
     const Response_t *res = conn_parse(conn);
 
     if (res != NULL) {
-        fprintf(stderr, "invalid response\n");
+        // fprintf(stderr, "invalid response\n");
         conn_send_response(conn, res);
     } else {
         debug("%s", conn_str(conn));
-        fprintf(stderr, "valid response\n");
+        // fprintf(stderr, "valid response\n");
 
         const Request_t *req = conn_get_request(conn);
         if (req == &REQUEST_GET) {
@@ -157,19 +154,17 @@ void handle_connection(int connfd) {
         }
     }
     conn_delete(&conn);
-
 }
 
 void handle_get(conn_t *conn) {
 
-
-    fprintf(stderr, "in get\n");
+    // fprintf(stderr, "in get\n");
 
     // pthread_mutex_lock(&mutex);
 
     int thread_id = 0;
     char *chid = conn_get_header(conn, "Request-Id");
-    if(chid != NULL) {
+    if (chid != NULL) {
         thread_id = atoi(chid);
     }
     //fprintf(stderr, "thread id is %d\n", thread_id);
@@ -178,7 +173,7 @@ void handle_get(conn_t *conn) {
     const Response_t *res = NULL; //here you initialized res
     debug("handling get request for %s", uri);
 
-    uint16_t statusCode;
+    int statusCode;
 
     // What are the steps in here?
 
@@ -193,25 +188,23 @@ void handle_get(conn_t *conn) {
         if (errno == EACCES) {
             res = &RESPONSE_FORBIDDEN;
             conn_send_response(conn, res);
-                        // pthread_mutex_unlock(&mutex);
+            // pthread_mutex_unlock(&mutex);
 
             goto out;
         } else if (errno == ENOENT) {
             res = &RESPONSE_NOT_FOUND;
             conn_send_response(conn, res);
-                        // pthread_mutex_unlock(&mutex);
+            // pthread_mutex_unlock(&mutex);
 
             goto out;
         } else {
             res = &RESPONSE_INTERNAL_SERVER_ERROR;
             conn_send_response(conn, res);
-                        // pthread_mutex_unlock(&mutex);
+            // pthread_mutex_unlock(&mutex);
 
             goto out;
-
         }
     }
-
 
     // 2. Get the size of the file.
     // (hint: checkout the function fstat)!
@@ -237,9 +230,11 @@ void handle_get(conn_t *conn) {
 
     // 4. Send the file
     // (hint: checkout the conn_send_file function!)
-    res = &RESPONSE_OK;
-    conn_send_response(conn, res);
-    conn_send_file(conn, fd, ssize);
+    res = conn_send_file(conn, fd, ssize);
+    if (res != NULL)
+        conn_send_response(conn, res);
+    else
+        res = &RESPONSE_OK;
 
 out:
     //sending response zone
@@ -247,21 +242,18 @@ out:
     audit("GET", uri, statusCode, thread_id);
 
     close(fd);
-
 }
 
 //FLOCK
 void handle_put(conn_t *conn) {
 
-    //fprintf(stderr, "WTF\n");
     int thread_id = 0;
     char *chid = conn_get_header(conn, "Request-Id");
-    if(chid != NULL) {
+    if (chid != NULL) {
         thread_id = atoi(chid);
     }
-    //fprintf(stderr, "thread id is %d\n", thread_id);
 
-    uint16_t statusCode;
+    int statusCode;
 
     char *uri = conn_get_uri(conn);
     const Response_t *res = NULL; //here you initialized res
@@ -276,7 +268,6 @@ void handle_put(conn_t *conn) {
 
     int fd = -1;
 
-
     // if (!existed) { // #2 create or truncate file
     //     fd = open(uri, O_CREAT | O_WRONLY | 0600);
     //     if (fd < 0) {
@@ -285,7 +276,7 @@ void handle_put(conn_t *conn) {
     //         goto out;
     //     }
 
-    // } else { 
+    // } else {
     //     fd = open(uri, O_TRUNC | O_WRONLY | 0600);
     //     if (fd < 0) {
     //         debug("%s: %d", uri, errno);
@@ -299,7 +290,10 @@ void handle_put(conn_t *conn) {
     //     }
     // }
 
-    fd = open(uri, O_CREAT | O_TRUNC | O_WRONLY, 0600); // #2 create or truncate file
+    fd = open(uri, O_CREAT | O_WRONLY, 0600); // #2 create or truncate file
+    flock(fd, LOCK_EX); //#3 flock on file
+    ftruncate(fd, 0); // truncate
+
     if (fd < 0) {
         debug("%s: %d", uri, errno);
         if (errno == EACCES || errno == EISDIR || errno == ENOENT) {
@@ -313,12 +307,9 @@ void handle_put(conn_t *conn) {
         }
     }
 
-
-    flock(fd, LOCK_EX); //#3 flock on file
-
     // #4 Release the file creation lock
     pthread_mutex_unlock(&mutex);
-    
+
     // #5 write
     res = conn_recv_file(conn, fd);
     if (res == NULL && existed) {
@@ -336,7 +327,6 @@ out:
 
     // #6 release flock on file
     close(fd);
-
 }
 
 void handle_unsupported(conn_t *conn) {
@@ -351,7 +341,6 @@ void handle_unsupported(conn_t *conn) {
     // if(chid != NULL) {
     //     thread_id = atoi(chid);
     // }
-
 
     // conn_send_response(conn, &RESPONSE_NOT_IMPLEMENTED);
 
